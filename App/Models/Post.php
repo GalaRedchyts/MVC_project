8ef51463bd9f81dbb0f5489d4250f373c8638ad1
GalaRedchyts <galynkared@gmail.com>
2022-07-20 16:32:51 +0300

    categories and post

---
+++ App/Models/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model;
+
+class Post extends Model
+{
+    protected static string|null $tableName = "posts";
+
+}
--- Config/functions.php
+++ Config/functions.php
@@ -1,7 +1,12 @@
 <?php
 
-function redirect(string $path = '/')
+function redirect(string $path = '')
 {
     header('Location:' . SITE_URL . '/' . $path);
     exit;
+}
+
+function url(string $link = ''): string
+{
+    return SITE_URL . '/' . $link;
 }
